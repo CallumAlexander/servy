e09f9f9acebaa99e5e6d5af0c4817328aa225dde
Callum Alexander <callumalexander.personal@gmail.com>
2023-10-30 21:52:38 +0000

    Now added different file location functionality

--- servy.hs
+++ servy.hs
@@ -5,10 +5,17 @@ import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
 
 app :: Application
-app _ respond = respond $ responseLBS
-    status200
-    [("Content-Type", "text/plain")]
-    "Hello, this is Servy"
+app req respond = case pathInfo req of
+    ["about"] -> respond $ responseLBS
+        status200 
+        [("Content-Type", "text/plain")]
+        "This is Servy. A webserver written in Haskell"
+    _ -> respond $ responseLBS
+        status404
+        [("Content-Type", "text/plain")]
+        "Page has not been found! Perhaps it does not exist"
+
+
 
 main :: IO () 
 main = do
